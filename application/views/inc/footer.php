77b3ec0e1ea9c0c4621a45f099ba786670a0dd8d
Maco Cortes <maco.techdepot@gmail.com>
2018-03-10 23:43:28 +0800

    Changed development settings

--- application/views/inc/footer.php
+++ application/views/inc/footer.php
@@ -2,6 +2,8 @@
     <strong>&copy; 2017 <a href="#">Cong Tibshrani</a></strong>. All rights
     reserved.
 </div>     	
+	<?php if(ENVIRONMENT == 'development'): ?>
     <span class="label bg-blue">{elapsed_time}s</span>
 	<span class="label bg-green">{memory_usage}</span>
+	<?php endif ?>
 	<span class="label bg-black">Your IP: <?=$_SERVER['REMOTE_ADDR']?></span>
